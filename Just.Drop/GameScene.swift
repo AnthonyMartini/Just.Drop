4034b275bdf579eae1608cbcc62571c4ef167408
Tony <Tonyboom123@gmail.com>
2021-05-04 21:43:36 -0400

    Imac

--- Just.Drop/GameScene.swift
+++ Just.Drop/GameScene.swift
@@ -15,13 +15,18 @@ class GameScene : CScene, SKPhysicsContactDelegate{
     var gameTimer = Timer()
     
     var ticks = 0
+    var score = 0
     var firsttouch = ""
     var jumpenabled = false
     var boostenabled = false
     
+    
+    
     //buttons
     var jumpButton = SKSpriteNode()
     var boostButton = SKSpriteNode()
+    
+    var scoreLabel = SKLabelNode()
     enum CategoryMask : UInt32 {
         case square = 0b00000001
         
@@ -34,7 +39,9 @@ class GameScene : CScene, SKPhysicsContactDelegate{
     override func didMove(to view: SKView) {
         super.didMove(to: view)
         self.physicsWorld.contactDelegate = self
-        
+        //Labels
+        scoreLabel = SKLabelNode(text: "0")
+        createLabel(scoreLabel, 70 * multiplier, CGPoint(x: 0, y: 440 * multiplier),font: "Verdana-Bold",color: .white)
         
         //Create square
         bounceSprite = SKSpriteNode()
@@ -75,7 +82,7 @@ class GameScene : CScene, SKPhysicsContactDelegate{
     
     @objc func GameUpdate(){
         ticks += 1
-        if ticks % 2 == 0{
+        if ticks % 1 == 0{
             createPiller(CGPoint(x: 462.5 * multiplier, y: -600 * multiplier),true)
         }
         
@@ -90,19 +97,29 @@ class GameScene : CScene, SKPhysicsContactDelegate{
         newpillar.physicsBody?.isDynamic = false
         newpillar.physicsBody?.categoryBitMask = CategoryMask.good.rawValue
         newpillar.physicsBody?.contactTestBitMask = CategoryMask.square.rawValue
-        newpillar.run(SKAction.sequence([SKAction.move(by: CGVector(dx: -900 * multiplier, dy: 800), duration: 6), SKAction.removeFromParent()]))
+        newpillar.run(SKAction.sequence([SKAction.move(by: CGVector(dx: -900 * multiplier, dy: 800), duration: 3), SKAction.removeFromParent()]))
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
         print("Contact")
+        let conA = contact.bodyA.node
+        let conB = contact.bodyB.node
+        
+        if conA?.name == "Piller" || conB?.name == "Piller"{
+            jumpenabled = true
+            score += 1
+            scoreLabel.text = String(score)
+            
+            
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches{
-            let pos = touch.location(in: self)
-            
+            let pos = touch.location(in: Cont)
             if jumpButton.contains(pos){
                 firsttouch = "Jump"
+
             }
             if boostButton.contains(pos){
                 firsttouch = "Boost"
@@ -113,10 +130,14 @@ class GameScene : CScene, SKPhysicsContactDelegate{
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches{
-            let pos = touch.location(in: self)
-            
+            let pos = touch.location(in: Cont)
             if jumpButton.contains(pos) && firsttouch == "Jump" && jumpenabled == true{
-                bounceSprite.physicsBody?.velocity = CGVector(dx: 0, dy: 100)
+                let up = SKAction.moveBy(x: 0, y: 400 * multiplier, duration: 0.4)
+                let spin = SKAction.rotate(byAngle: .pi / -2.00, duration: 0.25)
+                let upSpin = SKAction.group([up,spin])
+                
+                bounceSprite.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 700 * multiplier))
+                jumpenabled = false
             }
         }
     }
